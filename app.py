928fe20816f8ca963a802298e4245695e948e563
qamber-qsol <135946521+qamber-qsol@users.noreply.github.com>
2024-02-01 23:02:21 +0500

    Uploaded

---
+++ app.py
@@ -0,0 +1,17 @@
+
+from flask import Flask
+from flask import Blueprint , render_template, redirect, url_for, request, session, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+	return render_template('base.html')
+
+# main driver function
+if __name__ == '__main__':
+
+	# run() method of Flask class runs the application 
+	# on the local development server.
+	app.run(debug=True, host="localhost")
