b1eb78f9cef1ab430298e2412b2942b5708092c9
Qamber Ali <qamber.qsol@gmail.com>
2024-06-30 15:36:11 +0500

    Have changes

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+import csv 
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session ,jsonify
 from flask_mysqldb import MySQL
-
+from werkzeug.security import generate_password_hash, check_password_hash
 routes = Blueprint('routes', __name__)
 
 # Initialize MySQL
@@ -8,28 +9,45 @@ mysql = MySQL()
 
 @routes.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
-@routes.route('/add_item', methods=['POST'])
-def add_item():
+@routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        cur.close()
+        if user and check_password_hash(user['password'], password):
+            session['username'] = user['username']
+            flash('Login successful!')
+            return redirect(url_for('routes.index'))
+        else:
+            flash('Invalid credentials, please try again.')
+    return render_template('login.html')
+
+
+@routes.route('/add_user', methods=['GET', 'POST'])
+def add_user():
     if request.method == 'POST':
         name = request.form['name']
-        description = request.form['description']
-        price = request.form['price']
+        email = request.form['email']
+        password = request.form['password']
+        
+        hashed_password = generate_password_hash(password)
+        
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO menu_items (name, description, price) VALUES (%s, %s, %s)", (name, description, price))
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_password))
         mysql.connection.commit()
         cur.close()
-        flash('Item added successfully!')
+        
+        flash('User added successfully!', 'success')
         return redirect(url_for('routes.index'))
+    
+    return render_template('add_user.html')
 
-@routes.route('/menu')
-def menu():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM menu_items")
-    items = cur.fetchall()
-    cur.close()
-    return render_template('menu.html', items=items)
 
 @routes.route('/menudata',methods=['GET', 'POST'])
 def get_data():
